917453c6e396b4b9da65c6dda307b905511a1d5a
Daniil Beniash <daniilbeniash97@gmail.com>
2023-02-26 21:30:36 +0300

    add: title, icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
 import { Rubik } from '@next/font/google'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 const rubik = Rubik({subsets: ['latin'], display: 'swap'})
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <main className={rubik.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>vyvos-musora-minsk.by</title>
+        <link rel="shortcut icon" href="/logo.svg" type="image/svg"></link>
+      </Head>
+      <main className={rubik.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+    
     )
 }
 
